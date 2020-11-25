6bfca9d2123ec6371d8eb17bd17bc94b4d5c57e0
wellikson <wellikson@gmail.com>
2020-11-25 11:02:49 -0300

    Intro JUnit5

---
+++ src/test/java/com/mycompany/introjunit5/MathUtilTest.java
@@ -0,0 +1,31 @@
+package com.mycompany.introjunit5;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author wellikson
+ */
+ 
+class MathUtilTest {
+    
+    @Test
+    void testMdcAParP1() {
+        final int a = 6;
+        final int b = 3;
+        final int esperado = b;       
+        final int obtido=MathUtil.mdc(a, b); 
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testMdcAImparP1(){
+        final int a = 9;
+        final int b = 3;
+        final int esperado = b;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado,obtido);
+    }
+    
+}
