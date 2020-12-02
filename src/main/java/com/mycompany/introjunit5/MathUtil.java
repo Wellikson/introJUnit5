6219965ee5c99110a9567c7a6fef0334e845d436
wellikson <wellikson@gmail.com>
2020-12-02 09:46:34 -0300

    novas propriedades adicionadas

--- src/main/java/com/mycompany/introjunit5/MathUtil.java
+++ src/main/java/com/mycompany/introjunit5/MathUtil.java
@@ -7,10 +7,30 @@ package com.mycompany.introjunit5;
 public class MathUtil {
     
     public static int mdc(int a, int b){
+        //PROPRIEDADE 7
+        a = Math.abs(a);
+        b = Math.abs(b);
         
+        //PROPRIEDADE 6
+        final int maior = Math.max(a, b);
+        b = Math.min(a, b);
+        a = maior;
+        
+        //PROPRIEDADE 1
         if(b > 0 && a % b == 0){
             return b;
         }
+        
+        //PROPRIEDADE 3
+        if(b == 0){
+            return Math.abs(a);
+        }
+        
+        //PROPRIEDADE 5
+        if(a % b != 0){
+            return 1;
+        }
+        
         return -1;
     }
 }
